375aa497926e35db34248394df2c22d34307fc01
Christopher Aliprantis <aliprantis@proton.me>
2026-03-11 08:37:42 -0400

    bugfix

--- ToDo/App.xaml.cs
+++ ToDo/App.xaml.cs
@@ -3,7 +3,7 @@ using Uno.Resizetizer;
 
 namespace ToDo;
 
-public partial class App : Application
+public partial class App : Microsoft.UI.Xaml.Application
 {
     /// <summary>
     /// Initializes the singleton application object. This is the first line of authored code
