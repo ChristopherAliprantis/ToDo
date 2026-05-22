8fd4ca4c525b4ecb766a35abcda01e7feda5ddc4
Christopher Aliprantis <aliprantis@proton.me>
2026-05-22 07:35:25 -0400

    stuff

--- ToDo/Platforms/Desktop/Win32NotificationService.cs
+++ ToDo/Platforms/Desktop/Win32NotificationService.cs
@@ -51,7 +51,10 @@ public class Win32NotificationService : global::ToDo.INotificationService
         title = Sanitize(title);
         message = Sanitize(message);
         if (string.IsNullOrEmpty(title) && string.IsNullOrEmpty(message))
+        {    
             message = "";
+            title = "";
+        }
 
         // ensure small tag, or pass empty so native side won't set Tag
         if (string.IsNullOrEmpty(actionData)) actionData = string.Empty;
