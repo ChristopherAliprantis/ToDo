5fc965d1a5e9c7fa95714264ee0da0811ef0be11
Christopher Aliprantis <aliprantis@proton.me>
2026-07-10 10:38:55 +0300

    added a custom splashscreen for desktop

--- ToDo/StartUp.cs
+++ ToDo/StartUp.cs
@@ -4,6 +4,31 @@ public partial class Start : Page
 {
     Start()
     {
+#if DESKTOP || WINDOWS
+        var sp= new Microsoft.UI.Xaml.Media.Imaging.SvgImageSource(new Uri("ms-appx:///Assets/Splash/splash_screen"));
+        var splash= new Microsoft.UI.Xaml.Controls.Image
+        {
+            Source = sp,
+            Height = 40,
+            Width = 40,
+            Stretch = Microsoft.UI.Xaml.Media.Stretch.Uniform,
+            RenderTransformOrigin = new Windows.Foundation.Point(0.5, 0.5),
+            RenderTransform = rotationTransform
+            HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Center,
+            VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Center
+        };
+        this.Loaded +=(s,e)=>
+        {
+            splash.Height = this.ActualHeight / 2;
+            splash.Width = this.ActualWidth / 4;
+        };
+        this.SizeChanged += (s, e) =>
+        {
+            splash.Height = this.ActualHeight / 2;
+            splash.Width = this.ActualWidth / 4;
+        };
+        this.Content = splash;
+#endif
         App.rootFrame.Navigate(typeof(MainPage));
     }
 }
