25331e9f388a86814eddf12920e9fc530f6244fb
Christopher Aliprantis <aliprantis@proton.me>
2026-07-10 10:47:30 +0300

    stuff

--- ToDo/StartUp.cs
+++ ToDo/StartUp.cs
@@ -2,7 +2,7 @@ namespace ToDo;
 
 public partial class Start : Page
 {
-    Start()
+    public Start()
     {
 #if DESKTOP || WINDOWS
         var sp= new Microsoft.UI.Xaml.Media.Imaging.SvgImageSource(new Uri("ms-appx:///Assets/Splash/splash_screen"));
@@ -12,23 +12,23 @@ public partial class Start : Page
             Height = 40,
             Width = 40,
             Stretch = Microsoft.UI.Xaml.Media.Stretch.Uniform,
-            RenderTransformOrigin = new Windows.Foundation.Point(0.5, 0.5),
-            RenderTransform = rotationTransform,
             HorizontalAlignment = Microsoft.UI.Xaml.HorizontalAlignment.Center,
             VerticalAlignment = Microsoft.UI.Xaml.VerticalAlignment.Center
         };
-        this.Loaded +=(s,e)=>
-        {
-            splash.Height = this.ActualHeight / 2;
-            splash.Width = this.ActualWidth / 4;
-        };
-        this.SizeChanged += (s, e) =>
-        {
-            splash.Height = this.ActualHeight / 2;
-            splash.Width = this.ActualWidth / 4;
-        };
         this.Content = splash;
-        await Task.Delay(2500);
+        this.Loaded +=async(s,e)=>
+        {
+            splash.Height = this.ActualHeight / 2;
+            splash.Width = this.ActualWidth / 4;
+            await Task.Delay(2500);
+        };
+        this.SizeChanged += async(s, e) =>
+        {
+            splash.Height = this.ActualHeight / 2;
+            splash.Width = this.ActualWidth / 4;
+        };
+        
+        
 #endif
         App.rootFrame.Navigate(typeof(MainPage));
     }
