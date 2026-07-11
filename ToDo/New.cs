b36e660ea1c8833ed6b31e6e6e712123b7bea173
Christopher Aliprantis <aliprantis@proton.me>
2026-07-11 21:14:37 +0300

    stuff

--- ToDo/New.cs
+++ ToDo/New.cs
@@ -151,13 +151,13 @@ public sealed partial class New : Page // #if DESKTOP for all of skia desktop, #
             if (Date != DateOnly.MinValue)
             {
                 Date = Date.AddDays(-1);
-                date.Text = Date.ToString("yyyy-MM-dd");
+                date.Text = Date.ToString("d");
                 date.text.Text = date.Text;
             }
             else
             {
                 Date = DateOnly.MaxValue;
-                date.Text = Date.ToString("yyyy-MM-dd");
+                date.Text = Date.ToString("d");
                 date.text.Text = date.Text;
             }
         };
