3df35aa0e80e50f7b569626fe5d7a1ea7a31014c
Christopher Aliprantis <aliprantis@proton.me>
2026-07-17 17:58:14 +0300

    stuff

--- WinRT.cpp
+++ WinRT.cpp
@@ -4,8 +4,8 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Foundation.Collections.h>
 
-#include <windows.h>   // 1. Move this ABOVE tlhelp32.h
-#include <tlhelp32.h>  // 2. This can now safely use the Windows types
+#include <windows.h>   
+#include <tlhelp32.h>  
 
 #include <shobjidl.h>
 #include <shlobj.h>
@@ -23,346 +23,30 @@
 #pragma comment(lib, "shlwapi.lib")
 #pragma comment(lib, "windowsapp.lib")
 
-
 using namespace Microsoft::WRL;
 
 // =====================================================
-// APP ID
+// GLOBAL VARIABLES
 // =====================================================
 extern HINSTANCE g_hModuleInstance = nullptr;
-static constexpr wchar_t APP_ID[] =
-L"com.christopheraliprantis.todo";
-
 static std::wstring g_registeredAppId;
-
-// =====================================================
-// LOGGING
-// =====================================================
 
 static void DebugLog(const wchar_t* text)
 {
     OutputDebugStringW(text);
     OutputDebugStringW(L"\n");
-
-    try
-    {
-        std::wcerr << text << std::endl;
-    }
-    catch (...)
-    {
-    }
+    try { std::wcerr << text << std::endl; }
+    catch (...) {}
 }
 
 static void DebugHResult(const winrt::hresult_error& e)
 {
-    std::wstring msg =
-        L"[ToastDLL] HRESULT=0x" +
-        std::to_wstring(static_cast<uint32_t>(e.code())) +
-        L" MESSAGE=" +
-        std::wstring(e.message());
-
+    std::wstring msg = L"[ToastDLL] HRESULT=0x" + std::to_wstring(static_cast<uint32_t>(e.code())) + L" MESSAGE=" + std::wstring(e.message());
     DebugLog(msg.c_str());
 }
 
 // =====================================================
-// XML ESCAPE
-// =====================================================
-
-static std::wstring EscapeXml(winrt::hstring const& s)
-{
-    std::wstring in = s.c_str();
-    std::wstring out;
-
-    for (wchar_t c : in)
-    {
-        switch (c)
-        {
-        case L'&':  out += L"&amp;";  break;
-        case L'<':  out += L"&lt;";   break;
-        case L'>':  out += L"&gt;";   break;
-        case L'\'': out += L"&apos;"; break;
-        case L'"':  out += L"&quot;"; break;
-        default:    out += c;         break;
-        }
-    }
-
-    return out;
-}
-
-// =====================================================
-// CREATE TOAST XML
-// =====================================================
-
-static winrt::Windows::Data::Xml::Dom::XmlDocument CreateToastXml(
-    winrt::hstring const& title,
-    winrt::hstring const& message,
-    winrt::hstring const& launch = {})
-{
-    std::wstring safeTitle = EscapeXml(title);
-    std::wstring safeMessage = EscapeXml(message);
-
-    if (safeTitle.empty() && safeMessage.empty())
-    {
-        safeMessage = L" ";
-        safeTitle = L" ";
-    }
-
-    std::wstring xml;
-    std::wstring aumidStr = APP_ID; // "com.christopheraliprantis.todo"
-
-    if (launch.empty())
-    {
-        xml =
-            L"<toast appUserModelId=\"" + aumidStr + L"\">"
-            L"<visual>"
-            L"<binding template='ToastGeneric'>"
-            L"<text>" + safeTitle + L"</text>"
-            L"<text>" + safeMessage + L"</text>"
-            L"</binding>"
-            L"</visual>"
-            L"</toast>";
-    }
-    else
-    {
-        xml =
-            L"<toast launch=\"" + EscapeXml(launch) + L"\" appUserModelId=\"" + aumidStr + L"\">"
-            L"<visual>"
-            L"<binding template='ToastGeneric'>"
-            L"<text>" + safeTitle + L"</text>"
-            L"<text>" + safeMessage + L"</text>"
-            L"</binding>"
-            L"</visual>"
-            L"</toast>";
-    }
-
-    winrt::Windows::Data::Xml::Dom::XmlDocument doc;
-    doc.LoadXml(xml);
-    return doc;
-}
-
-
-// =====================================================
-// SHOW TOAST
-// =====================================================
-
-extern "C"
-{
-    __declspec(dllexport)
-        void __stdcall ShowToast(
-            const wchar_t* title,
-            const wchar_t* message)
-    {
-        try
-        {
-            std::wstring titleStr =
-                title
-                ? title
-                : L"";
-
-            std::wstring messageStr =
-                message
-                ? message
-                : L"";
-
-            auto doc =
-                CreateToastXml(
-                    winrt::hstring(titleStr),
-                    winrt::hstring(messageStr));
-
-            winrt::Windows::UI::Notifications::ToastNotification toast{ doc };
-
-            winrt::hstring notifierId =
-                g_registeredAppId.empty()
-                ? winrt::hstring(APP_ID)
-                : winrt::hstring(g_registeredAppId);
-
-            auto notifier =
-                winrt::Windows::UI::Notifications::ToastNotificationManager
-                ::CreateToastNotifier(notifierId);
-
-            notifier.Show(toast);
-
-            DebugLog(L"[ToastDLL] ShowToast succeeded");
-        }
-        catch (const winrt::hresult_error& e)
-        {
-            DebugHResult(e);
-        }
-        catch (...)
-        {
-            DebugLog(L"[ToastDLL] Unknown exception in ShowToast");
-        }
-    }
-}
-
-// =====================================================
-// SCHEDULE TOAST
-// =====================================================
-
-extern "C"
-{
-    __declspec(dllexport)
-        void __stdcall ScheduleToast(
-            const wchar_t* id,
-            const wchar_t* title,
-            const wchar_t* message,
-            int64_t fileTime)
-    {
-        try
-        {
-            FILETIME ftNow{};
-            GetSystemTimeAsFileTime(&ftNow);
-
-            ULARGE_INTEGER uli{};
-            uli.LowPart = ftNow.dwLowDateTime;
-            uli.HighPart = ftNow.dwHighDateTime;
-
-            uint64_t nowTicks = uli.QuadPart;
-
-            if ((uint64_t)fileTime <= nowTicks)
-            {
-                DebugLog(L"[ToastDLL] Scheduled time is in the past");
-                return;
-            }
-
-            uint64_t delta = (uint64_t)fileTime - nowTicks;
-
-            DebugLog(
-                (L"[ToastDLL] Delta ticks=" +
-                    std::to_wstring(delta)).c_str());
-
-            std::wstring titleStr =
-                title
-                ? title
-                : L"";
-
-            std::wstring messageStr =
-                message
-                ? message
-                : L"";
-
-            std::wstring tag = id;
-   
-
-            if (tag.length() > 63)
-            {
-                tag = tag.substr(0, 63);
-            }
-
-            auto doc =
-                CreateToastXml(
-                    winrt::hstring(titleStr),
-                    winrt::hstring(messageStr),
-                    winrt::hstring(tag));
-
-            winrt::file_time ft{ (uint64_t)fileTime };
-
-            winrt::Windows::Foundation::DateTime when = winrt::clock::from_file_time(ft);
-
-
-            winrt::Windows::UI::Notifications::ScheduledToastNotification toast
-            {
-                doc,
-                when
-            };
-
-            toast.Tag(winrt::hstring(tag));
-
-            winrt::hstring notifierId =
-                g_registeredAppId.empty()
-                ? winrt::hstring(APP_ID)
-                : winrt::hstring(g_registeredAppId);
-
-            auto notifier =
-                winrt::Windows::UI::Notifications::ToastNotificationManager
-                ::CreateToastNotifier(notifierId);
-
-            notifier.AddToSchedule(toast);
-
-            DebugLog(L"[ToastDLL] AddToSchedule succeeded");
-
-            auto scheduled =
-                notifier.GetScheduledToastNotifications();
-
-            DebugLog(
-                (L"[ToastDLL] Scheduled count=" +
-                    std::to_wstring(scheduled.Size())).c_str());
-        }
-        catch (const winrt::hresult_error& e)
-        {
-            DebugHResult(e);
-        }
-        catch (...)
-        {
-            DebugLog(L"[ToastDLL] Unknown exception in ScheduleToast");
-        }
-    }
-}
-
-// =====================================================
-// CANCEL TOAST
-// =====================================================
-
-extern "C"
-{
-    __declspec(dllexport)
-        void __stdcall CancelToast(
-            const wchar_t* id)
-    {
-        try
-        {
-            printf("Rct");
-            winrt::hstring targetTag(id);
-            winrt::hstring notifierId{ APP_ID };
-
-            auto notifier =
-                winrt::Windows::UI::Notifications::ToastNotificationManager
-                ::CreateToastNotifier(notifierId);
-
-            auto scheduled =
-                notifier.GetScheduledToastNotifications();
-
-            // 1. Initialize it as empty using std::nullopt
-            std::optional<winrt::Windows::UI::Notifications::ScheduledToastNotification> matchedToast = std::nullopt;
-
-            for (uint32_t i = 0; i < scheduled.Size(); ++i)
-            {
-                auto currentToast = scheduled.GetAt(i);
-
-                if (currentToast.Tag() == targetTag)
-                {
-                    // 2. Save the match and break immediately
-                    matchedToast = currentToast;
-                    break;
-                }
-            }
-
-            // 3. Check if the optional actually holds a value before using it
-            if (matchedToast.has_value())
-            {
-                notifier.RemoveFromSchedule(matchedToast.value());
-                printf("[ToastDLL] CancelToast succeeded");
-            }
-            else
-            {
-                winrt::hstring m = L"[ToastDLL] No matching scheduled toast found";
-                wprintf(L"%s\n", m.c_str());
-            }
-        }
-        catch (const winrt::hresult_error& e)
-        {
-            DebugHResult(e);
-        }
-        catch (...)
-        {
-            DebugLog(L"[ToastDLL] Unknown exception in CancelToast");
-        }
-    }
-}
-
-
-// =====================================================
-// REGISTER APP FOR TOASTS
+// REGISTRY UTILITIES
 // =====================================================
 bool SetRegistryString(HKEY hRootKey, const std::wstring& subKey, const std::wstring& valueName, const std::wstring& data) {
     HKEY hKey;
@@ -373,7 +57,6 @@ bool SetRegistryString(HKEY hRootKey, const std::wstring& subKey, const std::wst
     return result == ERROR_SUCCESS;
 }
 
-// Helper to write DWORD keys to HKCU
 bool SetRegistryDword(HKEY hRootKey, const std::wstring& subKey, const std::wstring& valueName, DWORD data) {
     HKEY hKey;
     LONG result = RegCreateKeyExW(hRootKey, subKey.c_str(), 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hKey, NULL);
@@ -383,64 +66,101 @@ bool SetRegistryDword(HKEY hRootKey, const std::wstring& subKey, const std::wstr
     return result == ERROR_SUCCESS;
 }
 
-// Bypasses Windows 11 cache bug by resetting user-level notification hosts
-// Bypasses Windows 11 cache bug by resetting user-level notification hosts
-
-
-
-xtern "C"
+// =====================================================
+// REGISTER APP FOR TOASTS
+// =====================================================
+extern "C"
 {
     __declspec(dllexport)
-        bool __stdcall RegisterAndVerifyToasts(const wchar_t* appId, const wchar_t* appName)
+        bool __stdcall RegisterAppForToasts(const wchar_t* appId, const wchar_t* appName)
     {
-        // 1. Initialize COM properly for Windows Runtime
-        HRESULT hr = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+        // 1. MUST use COINIT_MULTITHREADED for stable runtime shell service tracking
+        HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
         if (FAILED(hr) && hr != RPC_E_CHANGED_MODE)
         {
             return false;
         }
 
-        // 2. Explicitly bind the process ID 
+        wchar_t exePath[MAX_PATH]{};
+        GetModuleFileNameW(nullptr, exePath, MAX_PATH);
+
+        wchar_t startMenuPath[MAX_PATH]{};
+        hr = SHGetFolderPathW(nullptr, CSIDL_PROGRAMS, nullptr, 0, startMenuPath);
+        if (FAILED(hr)) return false;
+
+        std::wstring shortcutPath = std::wstring(startMenuPath) + L"\\" + appName + L".lnk";
+
+        ComPtr<IShellLinkW> shellLink;
+        hr = CoCreateInstance(CLSID_ShellLink, nullptr, CLSCTX_INPROC_SERVER, IID_PPV_ARGS(&shellLink));
+        if (FAILED(hr)) return false;
+
+        shellLink->SetPath(exePath);
+
+        // Dynamic Icon Binding: Pulls index 0 from current running executable
+        shellLink->SetIconLocation(exePath, 0);
+
+        ComPtr<IPropertyStore> propStore;
+        hr = shellLink.As(&propStore);
+        if (FAILED(hr)) return false;
+
+        PROPVARIANT pvAppId{};
+        InitPropVariantFromString(appId, &pvAppId);
+        hr = propStore->SetValue(PKEY_AppUserModel_ID, pvAppId);
+        PropVariantClear(&pvAppId);
+        if (FAILED(hr)) return false;
+
+        PROPVARIANT pvName{};
+        InitPropVariantFromString(appName, &pvName);
+        hr = propStore->SetValue(PKEY_ItemNameDisplay, pvName);
+        PropVariantClear(&pvName);
+        if (FAILED(hr)) return false;
+
+        hr = propStore->Commit();
+        if (FAILED(hr)) return false;
+
+        ComPtr<IPersistFile> persistFile;
+        hr = shellLink.As(&persistFile);
+        if (FAILED(hr)) return false;
+
+        hr = persistFile->Save(shortcutPath.c_str(), TRUE);
+        if (FAILED(hr)) return false;
+
         g_registeredAppId = appId;
+
         HRESULT setHr = SetCurrentProcessExplicitAppUserModelID(g_registeredAppId.c_str());
         if (FAILED(setHr)) {
-            DebugLog(L"[ToastDLL] Failed to set process AppUserModelID");
+            DebugLog(L"[ToastDLL] Failed to set AppUserModelID");
             return false;
         }
 
-        // Note: Keep your existing Start Menu Shortcut generation code here so the AppId is valid!
-        // [Insert your IShellLinkW shortcut generation code here]
+        std::wstring aumid = appId;
+        std::wstring classesPath = L"Software\\Classes\\AppUserModelId\\" + aumid;
+        std::wstring settingsPath = L"Software\\Microsoft\\Windows\\CurrentVersion\\Notifications\\Settings\\" + aumid;
 
+        // Populate Software\Classes directory rules
+        SetRegistryString(HKEY_CURRENT_USER, classesPath, L"DisplayName", appName);
+        SetRegistryString(HKEY_CURRENT_USER, classesPath, L"IconUri", exePath);
+        SetRegistryDword(HKEY_CURRENT_USER, classesPath, L"ShowBanners", 1);
+
+        // Populate user choice preferences subkey tree
+        SetRegistryDword(HKEY_CURRENT_USER, settingsPath, L"Enabled", 1);
+        SetRegistryDword(HKEY_CURRENT_USER, settingsPath, L"ShowInActionCenter", 1);
+        SetRegistryDword(HKEY_CURRENT_USER, settingsPath, L"ShowBanners", 1);
+
+        // Verification validation tracking engine lookup
         try
         {
-            // 3. Ask Windows directly what our current permission status is
             winrt::hstring notifierId(g_registeredAppId);
             auto notifier = winrt::Windows::UI::Notifications::ToastNotificationManager::CreateToastNotifier(notifierId);
-
-            // Read the official setting state directly from the Windows core engine
             auto setting = notifier.Setting();
 
-            switch (setting)
+            if (setting == winrt::Windows::UI::Notifications::NotificationSetting::Enabled)
             {
-            case winrt::Windows::UI::Notifications::NotificationSetting::Enabled:
-                DebugLog(L"[ToastDLL] Success! Notifications and Banners are fully ENABLED by the OS.");
-                return true;
-
-            case winrt::Windows::UI::Notifications::NotificationSetting::DisabledBySettings:
-                DebugLog(L"[ToastDLL] Blocked: User manually turned notifications off in the Settings UI.");
-                break;
-
-            case winrt::Windows::UI::Notifications::NotificationSetting::DisabledByGroupPolicy:
-                DebugLog(L"[ToastDLL] Blocked: Enterprise Group Policy is forcing this off.");
-                break;
-
-            case winrt::Windows::UI::Notifications::NotificationSetting::DisabledForUser:
-                DebugLog(L"[ToastDLL] Blocked: This specific Windows User account has notifications disabled.");
-                break;
-
-            case winrt::Windows::UI::Notifications::NotificationSetting::DisabledForApplication:
-                DebugLog(L"[ToastDLL] Blocked: App-level system restriction.");
-                break;
+                DebugLog(L"[ToastDLL] Verification Success: Notification Banners Active.");
+            }
+            else
+            {
+                DebugLog(L"[ToastDLL] Warning: OS notification service forced background-only restrictions.");
             }
         }
         catch (const winrt::hresult_error& e)
@@ -448,68 +168,19 @@ xtern "C"
             DebugHResult(e);
         }
 
-        return false;
+        return true;
     }
 }
 
-
-
-extern "C" __declspec(dllexport) bool __stdcall IsNotificationBlocked(const wchar_t* appId) {
-    std::wstring aumid = appId;
-
-    // 1. First, check if the Windows GLOBAL main switch is turned off
-    std::wstring globalPushPath = L"Software\\Microsoft\\Windows\\CurrentVersion\\PushNotifications";
-    HKEY hGlobalKey;
-    DWORD globalEnabled = 1; // Default to assuming it's on
-    DWORD dwSize = sizeof(DWORD);
-
-    if (RegOpenKeyExW(HKEY_CURRENT_USER, globalPushPath.c_str(), 0, KEY_READ, &hGlobalKey) == ERROR_SUCCESS) {
-        RegQueryValueExW(hGlobalKey, L"ToastEnabled", NULL, NULL, reinterpret_cast<LPBYTE>(&globalEnabled), &dwSize);
-        RegCloseKey(hGlobalKey);
-    }
-
-    // If the master Windows notification toggle is explicitly 0, it's blocked!
-    if (globalEnabled == 0) {
-        return true;
-    }
-
-    // 2. Next, check your individual app row settings
-    std::wstring settingsPath = L"Software\\Microsoft\\Windows\\CurrentVersion\\Notifications\\Settings\\" + aumid;
-    HKEY hAppKey;
-    LONG status = RegOpenKeyExW(HKEY_CURRENT_USER, settingsPath.c_str(), 0, KEY_READ, &hAppKey);
-
-    if (status != ERROR_SUCCESS) {
-        // The key doesn't even exist yet! This means your app is UNINITIALIZED, not blocked.
-        return false;
-    }
-
-    DWORD appEnabled = 1; // Assume enabled if key exists
-    dwSize = sizeof(DWORD);
-
-    // Query your specific app's toggle flag
-    status = RegQueryValueExW(hAppKey, L"Enabled", NULL, NULL, reinterpret_cast<LPBYTE>(&appEnabled), &dwSize);
-    RegCloseKey(hAppKey);
-
-    // If the key exists but the user intentionally set it to 0, it's blocked!
-    if (status == ERROR_SUCCESS && appEnabled == 0) {
-        return true;
-    }
-
-    return false; // Everything is clear!
-}
-
-
-BOOL WINAPI DllMain(
-    HINSTANCE hinstDLL,
-    DWORD fdwReason,
-    LPVOID lpvReserved)
+// =====================================================
+// MAIN DLL ENTRY POINT
+// =====================================================
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
 {
     switch (fdwReason)
     {
     case DLL_PROCESS_ATTACH:
         DisableThreadLibraryCalls(hinstDLL);
-
-        // 2. Assign the handle here
         g_hModuleInstance = hinstDLL;
         break;
     }
